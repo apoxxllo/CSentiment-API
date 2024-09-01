f6b0f9bfd9c8a98163cc776893ad2ec113f09187
apoxxllo <jilabrador019@gmail.com>
2024-09-01 13:31:27 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-chmod +x /workspace/wkhtmltopdf.exe
+apt-get update
+apt-get install -y wget
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+apt install -y ./wkhtmltox_0.12.6-1.bionic_amd64.deb
+
+# Clean up
+rm wkhtmltox_0.12.6-1.bionic_amd64.deb
